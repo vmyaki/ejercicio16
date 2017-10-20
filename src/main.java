7ec5aaca6676c7aca9cc20fffffd1aa4135b8505
vmyaki <wonka.vm@gmail.com>
2017-10-20 14:04:39 +0200

    subida a git

---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// Declaramos variables
+			int num=1;
+		
+		while(num<=50)
+		{
+			System.out.println(num);
+			num=num+1;
+		}
+
+	}
+
+}
